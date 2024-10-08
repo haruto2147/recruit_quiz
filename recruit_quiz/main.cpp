9755ee293880f268ba7fe13463bef15a94789b60
harut <harut@DESKTOP-7UL594Q>
2024-10-08 17:49:06 +0900

    プロジェクト ファイルを追加します。

---
+++ recruit_quiz/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "[リクルート試験対策クイズ]\n";
+	cout << "14x(-5)の答えは？\n";
+
+	int answer;
+	cin >> answer;
+
+	if (answer == 13 * -5)
+	{
+		cout << "正解！\n";
+	}
+	else
+	{
+		cout << "間違い！正解は" << 13 * -5 << "\n";
+	}
+}
