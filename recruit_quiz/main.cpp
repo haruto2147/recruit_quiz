c4b885f878d9a7808a69d1da83b716b87f6a7fcc
森迫 悠翔 <haruto6663@icloud.com>
2024-10-08 18:47:00 +0900

    コミット

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 //問題文と答えをまとめる構造体
 struct Question
 {
-	string s; //問題文
+	string q; //問題文
 	int a;	  //答え
 };
 
@@ -20,13 +20,13 @@ int main()
 	//掛け算
 	int x = uniform_int_distribution<>(1, 30)(rand);
 	int y = uniform_int_distribution<>(1, 20)(rand);
-	questions[0].s = to_string(x) + "x" + to_string(y);
+	questions[0].q = to_string(x) + "x" + to_string(y);
 	questions[0].a = x * y;
 
 	//割り算
 	x = uniform_int_distribution<>(1, 30)(rand);
 	y = uniform_int_distribution<>(1, 20)(rand);
-	questions[1].s = to_string(x) + "x" + to_string(y);
+	questions[1].q = to_string(x * y) + "x" + to_string(y);
 	questions[1].a = x / y;
 
 	//複雑な式
@@ -34,14 +34,14 @@ int main()
 	y = uniform_int_distribution<>(1, 10)(rand);
 	int z = uniform_int_distribution<>(1, 10)(rand);
 	int w = uniform_int_distribution<>(1, 10)(rand);
-	questions[2].s =
-		to_string(x) + "-(" + to_string(y) + "+" + to_string(z) + ")÷" + to_string(w);
-	questions[2].a = x - (y + z) / w;
+	questions[2].q =
+		to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w);
+	questions[2].a = x - (y + z);
 
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions) {
-		cout << e.s << "の答えは？\n";
+		cout << e.q << "の答えは？\n";
 		int answer;
 		cin >> answer;
 
